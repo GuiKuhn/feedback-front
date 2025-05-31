bb6729d5dbb3ccfa70e0810e93cc8306cee3137e
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-05-31 04:07:16 -0300

    pagina feedback

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes as ReactRoutes, BrowserRouter } from "react-router-dom";
 import { MainPage } from "../pages";
+import FeedbackSelection from "../pages/FeedbackSelection"; // 👈 novo
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <ReactRoutes>
-        <Route path="/" element={<MainPage></MainPage>} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/feedback" element={<FeedbackSelection />} /> {/* 👈 nova rota */}
       </ReactRoutes>
     </BrowserRouter>
   );
