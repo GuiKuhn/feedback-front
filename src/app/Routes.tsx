d36c3464a734f9f04f150fd6feb62931a1b120a6
Leo <leofwingert@gmail.com>
2025-05-31 05:43:15 -0300

    fix

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -14,7 +14,6 @@ const Routes = () => {
         <Route path="/feedback-details" element={<FeedbackDetails></FeedbackDetails>} />
         <Route path="/" element={<MainPage />} />
         <Route path="/feedback" element={<FeedbackSelection />} /> {/* 👈 nova rota */}
-        <Route path="/feedback-details" element={<FeedbackDetails></FeedbackDetails>} />
       </ReactRoutes>
     </BrowserRouter>
   );
