0c9b14d710abd7bda58e68807b63970f94a9cd50
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-05-31 04:07:16 -0300

    pagina feedback

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -12,6 +12,8 @@ const Routes = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/feedback" element={<FeedbackSelection />} /> {/* 👈 nova rota */}
         <Route path="/feedback-details" element={<FeedbackDetails></FeedbackDetails>} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/feedback" element={<FeedbackSelection />} /> {/* 👈 nova rota */}
       </ReactRoutes>
     </BrowserRouter>
   );
