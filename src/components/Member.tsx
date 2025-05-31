cbfdc285e1a4e3fe7e95591e6102c9289a74f94f
Samuel <samuel.bitdinger@gmail.com>
2025-05-31 05:38:46 -0300

    pagina de membros

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -1,10 +1,32 @@
 import { Card,CardTitle} from "./ui/card";
 import img from "../assets/download.jpeg"
+import { useNavigate } from "react-router-dom";
 
-function Member({name}:{name:string}){
+type User = {
+  name: string;
+  id: number;
+  photoUrl: string;
+}
+
+function Member({name,pg}:{name:string,pg:string}){
+  const navigate = useNavigate();
+
+  const members:User[] = [
+    {
+      name: "Samuel",
+      id: 1,
+      photoUrl: "aako",
+    },
+    {
+      name: "Bernardo",
+      id: 2,
+      photoUrl: "dakow",
+    }
+  ]
+
   return(
-    <Card className="text-center">
-      <img src={img} alt="" className="object-cover h-48 w-48 rounded-full mx-auto"/>
+    <Card className="text-center" onClick={()=>{navigate(pg)}}>
+      <img src={img} alt="" className="object-cover h-32 w-32 rounded-full mx-auto"/>
       <CardTitle>{name}</CardTitle>
     </Card>
   )
