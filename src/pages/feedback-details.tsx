68cfa66045a43e35e8bd03eec6511c91c3406d71
Samuel <samuel.bitdinger@gmail.com>
2025-05-31 11:47:24 -0300

    integracao com o back

--- src/pages/feedback-details.tsx
+++ src/pages/feedback-details.tsx
@@ -3,12 +3,40 @@ import { Switch } from "@/components/ui/switch";
 import BackButton from "@/components/BackButton";
 import ConfirmButton from "@/components/ConfirmButton";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 export const FeedbackDetails = () => {
   const navigate = useNavigate();
 
   const param = new URLSearchParams(window.location.search);
   const memberId = param.get("memberId");
+  const topics = param.get("feedbackId");
+
+  const [isAnonymous, setIsAnonymous] = useState(false);
+
+async function handleSubmit() {
+    try {
+      await axios.post("http://localhost:8080/feedback/", payload);
+      navigate("/confirmation");
+    } catch (error) {
+      console.error("Erro ao enviar feedback:", error);
+    }
+  };
+
+  const [payload, setPayload] = useState<{
+    idFromMember: number,
+    idToMember: number,
+    topics: number[],
+    message: string | null,
+    anonymous: boolean,
+  }>({
+    idFromMember: 0,
+    idToMember: 0,
+    topics: [],
+    anonymous: false,
+    message: null
+  })
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -17,6 +45,12 @@ export const FeedbackDetails = () => {
       </h1>
       <Textarea
         placeholder="Descreva aqui o que você gostaria de detalhar..."
+        onChange={(e) => {
+          setPayload(prev => {
+            prev.message = e.target.value
+            return prev
+          })
+        }}
         style={{
           padding: "0.75vw",
           fontSize: "1vw",
@@ -36,7 +70,11 @@ export const FeedbackDetails = () => {
         }}
       >
         <img src="./src/assets/anon-icon.svg" alt="" />
-        <Switch id="anon-mode" />
+        <Switch 
+          id="anon-mode" 
+          checked={isAnonymous}
+          onCheckedChange={setIsAnonymous}
+        />
         <label htmlFor="anon-mode">Enviar em modo anônimo</label>
       </div>
       <div
@@ -50,8 +88,20 @@ export const FeedbackDetails = () => {
         />
         <ConfirmButton
           onClick={() => {
-            navigate("/confirmation");
-          }}
+            setPayload(prev => {
+              if(memberId)
+                prev.idToMember = +memberId 
+
+              if(topics)
+                prev.topics = topics?.split(",").map(Number);
+
+              prev.anonymous = isAnonymous;
+              
+              return prev
+              })
+            console.log(payload)
+            handleSubmit()
+          }}  
         ></ConfirmButton>
       </div>
     </div>
