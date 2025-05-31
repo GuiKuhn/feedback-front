6f8ad33fb12cd689f13a4a51ba9b207f3baf1b6b
Leo <leofwingert@gmail.com>
2025-05-31 06:19:35 -0300

    fix feedback-details

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes as ReactRoutes, BrowserRouter } from "react-router-dom";
 import { MainPage } from "../pages";
 import FeedbackSelection from "../pages/FeedbackSelection"; // 👈 novo
+import { FeedbackDetails } from "@/pages/feedback-details";
 
 const Routes = () => {
   return (
@@ -8,6 +9,7 @@ const Routes = () => {
       <ReactRoutes>
         <Route path="/" element={<MainPage />} />
         <Route path="/feedback" element={<FeedbackSelection />} /> {/* 👈 nova rota */}
+        <Route path="/feedback-details" element={<FeedbackDetails />} />
       </ReactRoutes>
     </BrowserRouter>
   );
